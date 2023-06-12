3e11ea4df3438e66ed0e5e275eae3de7059cfa03
sebbyapudo <sebbyapudo@gmail.com>
2023-06-13 00:50:19 +0300

    Refactored code to make the functionality more dynamic and the code more concise

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -9,10 +9,5 @@ const btn = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 
 btn.addEventListener('click', ()=>{
-  if(!links.classList.contains('show-links')){
-    links.classList.add('show-links')
-  }
-  else {
-    links.classList.remove('show-links')
-  }
+  links.classList.toggle('show-links')
 })
