49ad40b91549eb08562dfa178dda7f19cedb886d
sebbyapudo <sebbyapudo@gmail.com>
2023-06-13 00:48:15 +0300

    Implemented the toggle functionality on the hamburger button

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,16 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+const btn = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+btn.addEventListener('click', ()=>{
+  if(!links.classList.contains('show-links')){
+    links.classList.add('show-links')
+  }
+  else {
+    links.classList.remove('show-links')
+  }
+})
