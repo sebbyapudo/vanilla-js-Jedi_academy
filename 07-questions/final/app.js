f8206ee77de30c6361eb06f46bdaf1ec51a59062
sebbyapudo <sebbyapudo@gmail.com>
2023-06-14 01:26:32 +0300

    Implemented the functionality that closes the text display of a question if it's not the current target

--- 07-questions/final/app.js
+++ 07-questions/final/app.js
@@ -6,6 +6,15 @@ questions.forEach((question)=>{
   const btn = question.querySelector('.question-btn');
   // console.log(btn);
   btn.addEventListener('click', ()=>{
+
+    questions.forEach((item)=>{
+      if(item !== question) {
+        item.classList.remove('show-text')
+      }
+      // console.log(item);
+    })
+
+
     question.classList.toggle('show-text')
   });
 });
