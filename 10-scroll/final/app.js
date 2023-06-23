5cfb9d7b326972f132071a37b727f67316abb48f
sebbyapudo <sebbyapudo@gmail.com>
2023-06-24 02:11:07 +0300

    Dynamically Implemented the toggle functionality on the menu button

--- 10-scroll/final/app.js
+++ 10-scroll/final/app.js
@@ -4,8 +4,27 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear()
+console.log(date);
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', () => {
+  // linksContainer.classList.toggle('show-links')
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if(containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`
+  }
+  else {
+    linksContainer.style.height = 0;
+  }
+})
+
 
 // ********** fixed navbar ************
 
