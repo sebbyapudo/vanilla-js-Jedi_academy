4e9d9d4032f7f2e8e99586fe3164d6d19f5fdb34
sebbyapudo <sebbyapudo@gmail.com>
2023-06-28 13:40:55 +0300

    Dynamically implemented the countdown in the in the deadline section

--- 12-countdown_timer/final/app.js
+++ 12-countdown_timer/final/app.js
@@ -40,3 +40,47 @@ let day = weekdays[futureDate.getDay()];
 console.log(day);
 giveaway.textContent = `Give away ends on ${day}, ${date}, ${month} ${year} ${hours}:${mins}am`;
 
+// Get time in miliseconds
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const currentTime = new Date().getTime();
+  const t = futureTime -currentTime
+  console.log(t);
+  // 1s --> 1000ms
+  // 1m --> 60s
+  // 1hr --> 60m
+  // 1day --> 24hr
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMin = 60 * 1000;
+  // calculate all values
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMin)
+  let seconds = Math.floor((t % oneMin) / 1000)
+
+  // set values array
+  const values = [days,hours,minutes,seconds];
+
+  function format(item) {
+    if(item < 10){
+      return item = `0${item}`
+    }
+    return item
+  }
+
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index])
+  })
+  if(t<0) {
+    clearInterval(countDown)
+    deadline.innerHTML = `<h4 class="expired">Sorry, this give away has expired!</h4>`
+  }
+}
+// coutdown
+let countDown = setInterval(getRemainingTime, 1000)
+getRemainingTime();
