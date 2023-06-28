3022680150ca68db54eee0043965163a52c52af9
sebbyapudo <sebbyapudo@gmail.com>
2023-06-28 12:46:39 +0300

    Implemented the end day & time of the giveaway dynamically

--- 12-countdown_timer/final/app.js
+++ 12-countdown_timer/final/app.js
@@ -21,3 +21,22 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4')
+
+let futureDate = new Date(2023, 6, 23, 11, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+let day = weekdays[futureDate.getDay()];
+console.log(day);
+giveaway.textContent = `Give away ends on ${day}, ${date}, ${month} ${year} ${hours}:${mins}am`;
+
