c205ceea85a4aacdd4755f38ef656b8283bb7f1a
sebbyapudo <sebbyapudo@gmail.com>
2023-06-28 13:52:53 +0300

    Dynamically set the countdown interval to 10 days from when the app is opened

--- 12-countdown_timer/final/app.js
+++ 12-countdown_timer/final/app.js
@@ -26,7 +26,14 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4')
 
-let futureDate = new Date(2023, 6, 23, 11, 30, 0);
+let tempDate = new Date();
+const tempYear = tempDate.getFullYear();
+const tempMonth = tempDate.getMonth();
+const tempDay = tempDate.getDate();
+
+
+// let futureDate = new Date(2023, 6, 23, 11, 30, 0);
+let futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0)
 console.log(futureDate);
 
 const year = futureDate.getFullYear();
